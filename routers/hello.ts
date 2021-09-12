b46688a2e21fb8fb75a9866e722a12c075a8f940
Akiacode <catry.me@gmail.com>
2021-09-12 21:02:31 +0900

    add endpoints

--- routers/hello.ts
+++ routers/hello.ts
@@ -1,5 +1,30 @@
 const hello = () => {
-    return new Response(JSON.stringify({ text: "Welcome to Packages API!" }), {
+    return new Response(JSON.stringify({
+        message: "Welcome to Packages API!",
+        endpoints: [
+            {
+                name: "Hello",
+                description: "Returns a message",
+                path: "/",
+            },
+            {
+                name: "Information",
+                description: "Returns information about the package",
+                path: "/info/:packageName",
+                params: [{
+                    lang: "Available langs: deno, nodejs, elixir, rust",
+                }]
+            },
+            {
+                name: "Search",
+                description: "Returns a list of packages",
+                path: "/search/:packageName",
+                params: [{
+                    lang: "Available langs: deno, nodejs, elixir, rust",
+                }]
+            }
+        ]
+    }), {
         status: 200,
         headers: { "content-type": "application/json" },
     });
